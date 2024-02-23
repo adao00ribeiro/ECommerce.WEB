30947d6d9c520f70758dcd0ef7e8a9e042991019
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-02-23 17:24:04 -0300

    fix: cardslider , home , footer

--- src/app/components/cardslider/cardslider.component.ts
+++ src/app/components/cardslider/cardslider.component.ts
@@ -1,26 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CarditemComponent } from '../carditem/carditem.component';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { bootstrapArrowLeftCircle ,bootstrapArrowRightCircle} from '@ng-icons/bootstrap-icons'
+import { bootstrapArrowLeftCircle, bootstrapArrowRightCircle } from '@ng-icons/bootstrap-icons'
 import { IProduct } from '../../mock/IProduct';
 
 @Component({
   selector: 'app-cardslider',
   standalone: true,
-  imports: [CarditemComponent,NgIconComponent],
-  viewProviders: [provideIcons({bootstrapArrowLeftCircle,bootstrapArrowRightCircle })],
+  imports: [NgIconComponent],
+  viewProviders: [provideIcons({ bootstrapArrowLeftCircle, bootstrapArrowRightCircle })],
   templateUrl: './cardslider.component.html',
   styleUrl: './cardslider.component.scss'
 })
 export class CardsliderComponent implements OnInit {
 
-  @Input()
-  products : IProduct[] = []
 
   currentIndex = 0;
 
   ngOnInit(): void {
-    setInterval(() => this.nextSlide(), 5000); // Optional automatic slide change
+
   }
   prevSlide() {
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,52 +5,53 @@ import { CarouselComponent } from '../../components/carousel/carousel.component'
 import { CarditemComponent } from '../../components/carditem/carditem.component';
 import { CardsliderComponent } from '../../components/cardslider/cardslider.component';
 import { IProduct } from '../../mock/IProduct';
+import { BrandsComponent } from '../../components/brands/brands.component';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CarouselComponent, OurserviceComponent, CardsliderComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    imports: [CarouselComponent, OurserviceComponent, CardsliderComponent, CarditemComponent, BrandsComponent],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
-   productNews: IProduct[] = [
-    {
-        id: 1,
-        name: "Camiseta básica",
-        price: 19.99,
-        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
-    },
-    {
-        id: 2,
-        name: "Calça jeans",
-        price: 39.99,
-        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
-    },
-    {
-        id: 3,
-        name: "Tênis esportivo",
-        price: 59.99,
-        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
-    },
-    {
-        id: 4,
-        name: "Relógio de pulso",
-        price: 79.99,
-        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
-    },
-    {
-        id: 5,
-        name: "Bolsa de couro",
-        price: 99.99,
-        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
-    },
-    {
-        id: 6,
-        name: "Óculos de sol",
-        price: 29.99,
-        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
-    }
-];
+    productNews: IProduct[] = [
+        {
+            id: 1,
+            name: "Camiseta básica",
+            price: 19.99,
+            imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+        },
+        {
+            id: 2,
+            name: "Calça jeans",
+            price: 39.99,
+            imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+        },
+        {
+            id: 3,
+            name: "Tênis esportivo",
+            price: 59.99,
+            imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+        },
+        {
+            id: 4,
+            name: "Relógio de pulso",
+            price: 79.99,
+            imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+        },
+        {
+            id: 5,
+            name: "Bolsa de couro",
+            price: 99.99,
+            imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+        },
+        {
+            id: 6,
+            name: "Óculos de sol",
+            price: 29.99,
+            imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+        }
+    ];
 }
