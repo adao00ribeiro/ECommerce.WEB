58256d3c2006cdd48a401a44b91d8bee1e6313b3
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-02-22 22:17:35 -0300

    fix: wishlist

--- src/app/pages/wishlist/wishlist.component.ts
+++ src/app/pages/wishlist/wishlist.component.ts
@@ -11,5 +11,42 @@ import { CarditemComponent } from '../../components/carditem/carditem.component'
   styleUrl: './wishlist.component.scss'
 })
 export class WishlistComponent {
-  products : IProduct[] = []
+  products: IProduct[] = [
+    {
+        id: 1,
+        name: "Camiseta básica",
+        price: 19.99,
+        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+    },
+    {
+        id: 2,
+        name: "Calça jeans",
+        price: 39.99,
+        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+    },
+    {
+        id: 3,
+        name: "Tênis esportivo",
+        price: 59.99,
+        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+    },
+    {
+        id: 4,
+        name: "Relógio de pulso",
+        price: 79.99,
+        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+    },
+    {
+        id: 5,
+        name: "Bolsa de couro",
+        price: 99.99,
+        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+    },
+    {
+        id: 6,
+        name: "Óculos de sol",
+        price: 29.99,
+        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+    }
+];
 }
