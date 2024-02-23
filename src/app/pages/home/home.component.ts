5bacc3430c09b2f9f8fdfe833b32fc323d792340
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-02-22 22:17:21 -0300

    fix : home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
 import { CarditemComponent } from '../../components/carditem/carditem.component';
 import { CardsliderComponent } from '../../components/cardslider/cardslider.component';
+import { IProduct } from '../../mock/IProduct';
 
 @Component({
   selector: 'app-home',
@@ -14,4 +15,42 @@ import { CardsliderComponent } from '../../components/cardslider/cardslider.comp
 })
 export class HomeComponent {
 
+   productNews: IProduct[] = [
+    {
+        id: 1,
+        name: "Camiseta básica",
+        price: 19.99,
+        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+    },
+    {
+        id: 2,
+        name: "Calça jeans",
+        price: 39.99,
+        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+    },
+    {
+        id: 3,
+        name: "Tênis esportivo",
+        price: 59.99,
+        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+    },
+    {
+        id: 4,
+        name: "Relógio de pulso",
+        price: 79.99,
+        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+    },
+    {
+        id: 5,
+        name: "Bolsa de couro",
+        price: 99.99,
+        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+    },
+    {
+        id: 6,
+        name: "Óculos de sol",
+        price: 29.99,
+        imageUrl: "https://m.media-amazon.com/images/I/417S18ZulVL._AC_SX679_.jpg"
+    }
+];
 }
