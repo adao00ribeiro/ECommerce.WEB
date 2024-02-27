14c592f7be3673af46e5d4a718e25b78d517fdb1
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-02-27 14:55:36 -0300

    fix: panelleft , dashboard , buttonmenucss

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { PanelleftComponent } from '../../components/dashboard/panelleft/panelleft.component';
 
+import { RouterOutlet, provideRouter } from '@angular/router';
+
+import { HomedashboardComponent } from '../../components/dashboard/homedashboard/homedashboard.component';
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [PanelleftComponent],
+  imports: [PanelleftComponent, RouterOutlet, HomedashboardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
