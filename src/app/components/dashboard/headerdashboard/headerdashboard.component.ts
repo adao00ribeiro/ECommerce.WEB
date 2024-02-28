a0effccf09b2e90e30adf5eca92b87be98005eae
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-02-28 15:26:43 -0300

    feat: headerdashboard

---
+++ src/app/components/dashboard/headerdashboard/headerdashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headerdashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './headerdashboard.component.html',
+  styleUrl: './headerdashboard.component.scss'
+})
+export class HeaderdashboardComponent {
+
+}
