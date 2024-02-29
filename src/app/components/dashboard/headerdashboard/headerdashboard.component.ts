02111f2be4f6ffcf11bdccce203dc12aacfe5699
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-02-28 21:51:36 -0300

    fix: headerdashboard

--- src/app/components/dashboard/headerdashboard/headerdashboard.component.ts
+++ src/app/components/dashboard/headerdashboard/headerdashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { ionNotifications} from '@ng-icons/ionicons'
 @Component({
   selector: 'app-headerdashboard',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
+  viewProviders: [provideIcons({ ionNotifications})],
   templateUrl: './headerdashboard.component.html',
   styleUrl: './headerdashboard.component.scss'
 })
