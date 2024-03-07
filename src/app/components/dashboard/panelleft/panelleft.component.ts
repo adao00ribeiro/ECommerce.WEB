4fc068e602071e95d26ca419b14b938f07090a95
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-03-07 10:41:27 -0300

    fix: routes dashboard , css

--- src/app/components/dashboard/panelleft/panelleft.component.ts
+++ src/app/components/dashboard/panelleft/panelleft.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { ButtonmenuComponent } from '../buttonmenu/buttonmenu.component';
 import { faGuitar, faHome, faMusic, faSearch } from '@fortawesome/free-solid-svg-icons';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { bootstrapGrid } from '@ng-icons/bootstrap-icons'
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 @Component({
   selector: 'app-panelleft',
   standalone: true,
-  imports: [ButtonmenuComponent, NgIconComponent],
+  imports: [ButtonmenuComponent, NgIconComponent, RouterLink],
   viewProviders: [provideIcons({ bootstrapGrid })],
   templateUrl: './panelleft.component.html',
   styleUrl: './panelleft.component.scss'
@@ -28,9 +28,8 @@ export class PanelleftComponent {
 
   }
 
-  botaoClick(botao: string) {
-
-    this.router.navigateByUrl('player/home');
+  botaoClick(rota: string) {
+    this.router.navigateByUrl(rota);
   }
 
   colapseClick(id: string) {
