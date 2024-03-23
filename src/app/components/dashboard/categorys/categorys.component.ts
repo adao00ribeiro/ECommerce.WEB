12c0afa126dec092a6ca3ce092e7565647bc130a
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-03-22 21:56:13 -0300

    fix

--- src/app/components/dashboard/categorys/categorys.component.ts
+++ src/app/components/dashboard/categorys/categorys.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../../services/category/category.service';
+import { ICategory } from '../../../Interface/ICategory';
 @Component({
   selector: 'app-categorys',
   standalone: true,
@@ -9,12 +10,22 @@ import { CategoryService } from '../../../services/category/category.service';
 })
 export class CategorysComponent implements OnInit {
 
+  categorys : ICategory[] = [];
 
   constructor(private categoryService: CategoryService) {
 
   }
   ngOnInit(): void {
-    this.categoryService.GetAll().then(data => console.log(data));
+    this.categoryService.GetAll()
+    .then(data =>  this.categorys = [{ id: "1", name: 'Tecnologia', slug: 'tecnologia' },
+    { id: "2", name: 'Esportes', slug: 'esportes' },
+    { id: "3", name: 'Entretenimento', slug: 'entretenimento' },
+    { id: "4", name: 'Saúde e Bem-estar', slug: 'saude-bem-estar' },
+    { id: "5", name: 'Moda', slug: 'moda' }])
+
+    ;
+
+
   }
 
 
